33467da07d038eec3d57c89d62b930c75f9a5c65
이서연 <75468561+truebird2201@users.noreply.github.com>
2023-01-04 21:20:55 +0900

    GameFramework 추가 중

--- NonSense/NonSense/stdafx.h
+++ NonSense/NonSense/stdafx.h
@@ -15,3 +15,26 @@
 #include <tchar.h>
 #include <chrono>
 #include <list>
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <DXGIDebug.h>
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+using Microsoft::WRL::ComPtr;
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#define FRAME_BUFFER_WIDTH 800
+#define FRAME_BUFFER_HEIGHT 
