e4843eab5c4fb6d3f196a8f373f6db95db6843c9
이서연 <75468561+truebird2201@users.noreply.github.com>
2023-01-07 15:33:23 +0900

    인덱스 버퍼를 활용한 육면체

--- NonSense/NonSense/Mesh.h
+++ NonSense/NonSense/Mesh.h
@@ -11,25 +11,26 @@ public:
 	Vertex(XMFLOAT3 xmf3Position) { m_xmf3Position = xmf3Position; }
 	~Vertex() { }
 };
-class CDiffusedVertex : public Vertex
+
+class DiffusedVertex : public Vertex
 {
 protected:
 	//정점의 색상이다.
 	XMFLOAT4 m_xmf4Diffuse;
 public:
-	CDiffusedVertex() {
+	DiffusedVertex() {
 		m_xmf3Position = XMFLOAT3(0.0f, 0.0f, 0.0f); m_xmf4Diffuse =
 			XMFLOAT4(0.0f, 0.0f, 0.0f, 0.0f);
 	}
-	CDiffusedVertex(float x, float y, float z, XMFLOAT4 xmf4Diffuse) {
+	DiffusedVertex(float x, float y, float z, XMFLOAT4 xmf4Diffuse) {
 		m_xmf3Position =
 			XMFLOAT3(x, y, z); m_xmf4Diffuse = xmf4Diffuse;
 	}
-	CDiffusedVertex(XMFLOAT3 xmf3Position, XMFLOAT4 xmf4Diffuse) {
+	DiffusedVertex(XMFLOAT3 xmf3Position, XMFLOAT4 xmf4Diffuse) {
 		m_xmf3Position =
 			xmf3Position; m_xmf4Diffuse = xmf4Diffuse;
 	}
-	~CDiffusedVertex() { }
+	~DiffusedVertex() { }
 };
 
 
@@ -47,12 +48,24 @@ public:
 protected:
 	ID3D12Resource* m_pd3dVertexBuffer = NULL;
 	ID3D12Resource* m_pd3dVertexUploadBuffer = NULL;
+	ID3D12Resource* m_pd3dIndexBuffer = NULL;
+	ID3D12Resource* m_pd3dIndexUploadBuffer = NULL;
+	/*인덱스 버퍼(인덱스의 배열)와 인덱스 버퍼를 위한 업로드 버퍼에 대한 인터페이스 포인터이다. 인덱스 버퍼는 정점
+	버퍼(배열)에 대한 인덱스를 가진다.*/
 	D3D12_VERTEX_BUFFER_VIEW m_d3dVertexBufferView;
+	D3D12_INDEX_BUFFER_VIEW m_d3dIndexBufferView;
 	D3D12_PRIMITIVE_TOPOLOGY m_d3dPrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 	UINT m_nSlot = 0;
 	UINT m_nVertices = 0;
 	UINT m_nStride = 0;
 	UINT m_nOffset = 0;
+	UINT m_nIndices = 0;
+	//인덱스 버퍼에 포함되는 인덱스의 개수이다.
+	UINT m_nStartIndex = 0;
+	//인덱스 버퍼에서 메쉬를 그리기 위해 사용되는 시작 인덱스이다.
+	int m_nBaseVertex = 0;
+	//인덱스 버퍼의 인덱스에 더해질 인덱스이다.
+
 public:
 	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList);
 };
@@ -68,7 +81,6 @@ class CubeMesh : public Mesh
 {
 public:
 	//직육면체의 가로, 세로, 깊이의 길이를 지정하여 직육면체 메쉬를 생성한다.
-	CubeMesh(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList
-	*pd3dCommandList, float fWidth = 2.0f, float fHeight = 2.0f, float fDepth = 2.0f);
+	CubeMesh(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, float fWidth = 2.0f, float fHeight = 2.0f, float fDepth = 2.0f);
 	virtual ~CubeMesh();
 };
