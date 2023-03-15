8a8f1cefc60e0e4c6920b395e6233de75085cee0
JeaWoo <55945935+JeaWoo-Park@users.noreply.github.com>
2023-03-15 20:10:23 +0900

    여러 오브젝트 띄우기(5개)

--- NonSense/NonSense/GameScene.cpp
+++ NonSense/NonSense/GameScene.cpp
@@ -114,15 +114,39 @@ void GameScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
 	//m_pShaders[0].BuildObjects(pd3dDevice, pd3dCommandList);
 
 
-	m_nObjects = 1;
+	m_nObjects = 5;
 	m_GameObjects = new Object * [m_nObjects];
 
-	LoadedModelInfo* pModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/goblin_Far.bin", NULL);
-	//LoadedModelInfo* pWeaponModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/Wand.bin", NULL);
+	LoadedModelInfo* pModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/F05.bin", NULL);
+	LoadedModelInfo* pWeaponModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/Wand.bin", NULL);
 
-	m_GameObjects[0] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pModel, NULL, 1);
+	m_GameObjects[0] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pModel, pWeaponModel, 1);
 	m_GameObjects[0]->m_pSkinnedAnimationController->SetTrackAnimationSet(0, 4);
 	m_GameObjects[0]->SetPosition(0.0f, 0.0f, 0.0f);
+
+	pModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/goblin_Far.bin", NULL);
+	m_GameObjects[1] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pModel, NULL, 1);
+	m_GameObjects[1]->m_pSkinnedAnimationController->SetTrackAnimationSet(0, 1);
+	m_GameObjects[1]->SetPosition(1.0f, 0.0f, 0.0f);
+	
+	pModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/goblin_Close.bin", NULL);
+
+	m_GameObjects[2] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pModel, NULL, 1);
+	m_GameObjects[2]->m_pSkinnedAnimationController->SetTrackAnimationSet(0, 6);
+	m_GameObjects[2]->SetPosition(2.0f, 0.0f, 0.0f);
+
+	pModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/goblin_Rush.bin", NULL);
+
+	m_GameObjects[3] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pModel, NULL, 1);
+	m_GameObjects[3]->m_pSkinnedAnimationController->SetTrackAnimationSet(0, 5);
+	m_GameObjects[3]->SetPosition(-1.0f, 0.0f, 0.0f);
+
+	pModel = Object::LoadAnimationModel(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, "Model/ent.bin", NULL);
+
+	m_GameObjects[4] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pModel, NULL, 1);
+	m_GameObjects[4]->m_pSkinnedAnimationController->SetTrackAnimationSet(0, 3);
+	m_GameObjects[4]->SetPosition(-3.0f, 0.0f, 0.0f);
+
 
 	//m_GameObjects[0] = new TestModelObject(pd3dDevice, pd3dCommandList, m_pGraphicsRootSignature, pWeaponModel, NULL, 0);
 	//m_GameObjects[0]->SetPosition(0.0f, 5.0f, 0.0f);
