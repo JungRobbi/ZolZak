2cfa12ec5adfeb15c60919c17210526b72ad241a
JeaWoo <55945935+JeaWoo-Park@users.noreply.github.com>
2023-04-19 03:40:11 +0900

    FEAT : 1인칭 추가 FIX : 쵀대 속도에 도달하면 속도가 갑자기 줄어드는 버그 수정

--- NonSense/NonSense/Object.h
+++ NonSense/NonSense/Object.h
@@ -379,6 +379,7 @@ public:
 	virtual void Animate(float fTimeElapsed);
 	virtual void OnPrepareRender();
 	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, Camera* pCamera);
+	virtual void RenderOnlyOneFrame(ID3D12GraphicsCommandList* pd3dCommandList, Camera* pCamera);
 	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, Camera* pCamera, UINT nInstances, D3D12_VERTEX_BUFFER_VIEW d3dInstancingBufferView);
 	//상수 버퍼를 생성한다.
 	virtual void CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
