0ef9b5130af0fc0008850e5932840786bc62ee23
Jung Rob bi <ziqqagwi9423@gmail.com>
2023-04-19 15:09:31 +0900

    FEAT: Timer 추가

--- NonSense/NonSense_Server/Components/PlayerMovementComponent.cpp
+++ NonSense/NonSense_Server/Components/PlayerMovementComponent.cpp
@@ -6,7 +6,19 @@
 void PlayerMovementComponent::start()
 {
 	scene = dynamic_cast<Scene*>(Scene::scene);
-	
+
+	m_xmf3Position = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	m_xmf3Right = XMFLOAT3(1.0f, 0.0f, 0.0f);
+	m_xmf3Up = XMFLOAT3(0.0f, 1.0f, 0.0f);
+	m_xmf3Look = XMFLOAT3(0.0f, 0.0f, 1.0f);
+	m_xmf3Velocity = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	m_xmf3Gravity = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	m_fMaxVelocityXZ = 0.0f;
+	m_fMaxVelocityY = 0.0f;
+	m_fFriction = 0.0f;
+	m_fPitch = 0.0f;
+	m_fRoll = 0.0f;
+	m_fYaw = 0.0f;
 }
 
 void PlayerMovementComponent::update()
@@ -25,4 +37,47 @@ void PlayerMovementComponent::update()
 
 	}
 
+}
+
+void PlayerMovementComponent::Move(DWORD dwDirection, float fDistance, bool bUpdateVelocity)
+{
+	if (dwDirection)
+	{
+		XMFLOAT3 xmf3Shift = XMFLOAT3(0, 0, 0);
+		if (dwDirection & DIR_FORWARD) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, fDistance);
+		if (dwDirection & DIR_BACKWARD) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, -fDistance);
+		if (dwDirection & DIR_RIGHT) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, fDistance);
+		if (dwDirection & DIR_LEFT) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, -fDistance);
+		if (dwDirection & DIR_UP) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Up, fDistance);
+		if (dwDirection & DIR_DOWN) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Up, -fDistance);
+		Move(xmf3Shift, bUpdateVelocity);
+	}
+}
+void PlayerMovementComponent::Move(XMFLOAT3& xmf3Shift, bool bUpdateVelocity)
+{
+	if (bUpdateVelocity)
+	{
+		m_xmf3Velocity = Vector3::Add(m_xmf3Velocity, xmf3Shift);
+	}
+	else
+	{
+		m_xmf3Position = Vector3::Add(m_xmf3Position, xmf3Shift);
+	}
+}
+
+void PlayerMovementComponent::updateValocity()
+{
+	m_xmf3Velocity = Vector3::Add(m_xmf3Velocity, Vector3::ScalarProduct(m_xmf3Gravity, fTimeElapsed, false));
+	float fLength = sqrtf(m_xmf3Velocity.x * m_xmf3Velocity.x + m_xmf3Velocity.z * m_xmf3Velocity.z);
+	float fMaxVelocityXZ = m_fMaxVelocityXZ * fTimeElapsed;
+	if (fLength > m_fMaxVelocityXZ)
+	{
+		m_xmf3Velocity.x *= (fMaxVelocityXZ / fLength);
+		m_xmf3Velocity.z *= (fMaxVelocityXZ / fLength);
+	}
+	float fMaxVelocityY = m_fMaxVelocityY * fTimeElapsed;
+	fLength = sqrtf(m_xmf3Velocity.y * m_xmf3Velocity.y);
+	if (fLength > m_fMaxVelocityY) m_xmf3Velocity.y *= (fMaxVelocityY / fLength);
+	XMFLOAT3 xmf3Velocity = Vector3::ScalarProduct(m_xmf3Velocity, fTimeElapsed, false);
+	Move(xmf3Velocity, false);
 }
--- NonSense/NonSense_Server/Components/PlayerMovementComponent.h
+++ NonSense/NonSense_Server/Components/PlayerMovementComponent.h
@@ -6,8 +6,35 @@ class PlayerMovementComponent :
     public Component
 {
     class Scene* scene;
+
+	XMFLOAT3 m_xmf3Position;
+	XMFLOAT3 m_xmf3Right;
+	XMFLOAT3 m_xmf3Up;
+	XMFLOAT3 m_xmf3Look;
+	float m_fPitch;
+	float m_fYaw;
+	float m_fRoll;
+	XMFLOAT3 m_xmf3Velocity;
+	XMFLOAT3 m_xmf3Gravity;
+	float m_fMaxVelocityXZ;
+	float m_fMaxVelocityY;
+	float m_fFriction;
+
 public:
     void start();
     void update();
+
+	XMFLOAT3 GetPosition() { return(m_xmf3Position); }
+	XMFLOAT3 GetLookVector() { return(m_xmf3Look); }
+	XMFLOAT3 GetUpVector() { return(m_xmf3Up); }
+	XMFLOAT3 GetRightVector() { return(m_xmf3Right); }
+
+	XMFLOAT3 GetVelocity() { return(m_xmf3Velocity); }
+	float GetYaw() { return(m_fYaw); }
+	float GetPitch() { return(m_fPitch); }
+	float GetRoll() { return(m_fRoll); }
+
+	void Move(DWORD nDirection, float fDistance, bool bVelocity = false);
+	void Move(XMFLOAT3& xmf3Shift, bool bVelocity = false);
 };
 
